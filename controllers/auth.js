1439b3f83033531528f84ee0e0e7c544e5229e10
Surajit Basak <surajitbasak109@gmail.com>
2020-12-18 00:23:15 +0530

    Login auth controller implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,25 +14,67 @@ exports.register = (req, res) => {
 
     if (results.length > 0) {
       return res.render('register', {
-        message: { type: 'danger', text: 'That email is already in use' }
+        message: { type: 'danger', text: 'That email is already in use' },
       })
     } else if (password !== passwordConfirmation) {
       return res.render('register', {
-        message: { type: 'danger', text: 'Password do not match' }
+        message: { type: 'danger', text: 'Password do not match' },
       })
     }
 
     const hashedPassword = await bcrypt.hash(password, 8)
 
-    db.query('INSERT INTO users (name, email, password, created_at) VALUES (?, ?, ?, NOW())', [name, email, hashedPassword], (error, results) => {
-      if (error) {
-        console.log(error)
-      } else {
-        console.log(results)
-        return res.render('register', {
-          message: { type: 'success', text: 'User registered' }
+    db.query(
+      'INSERT INTO users (name, email, password, created_at) VALUES (?, ?, ?, NOW())',
+      [name, email, hashedPassword],
+      (error, results) => {
+        if (error) {
+          console.log(error)
+        } else {
+          console.log(results)
+          return res.render('register', {
+            message: { type: 'success', text: 'User registered' },
+          })
+        }
+      }
+    )
+  })
+}
+
+exports.login = (req, res) => {
+  try {
+    const { email, password } = req.body
+    if (!email || !password) {
+      return res.status(400).render('login', {
+        message: { type: 'danger', text: 'Please provide an email and password' },
+      })
+    }
+
+    db.query('SELECT * FROM users WHERE email = ?', [email], async (error, results) => {
+      console.log(results)
+      if (!results.length || !(await bcrypt.compare(password, results[0].password))) {
+        res.status(401).render('login', {
+          message: { type: 'danger', text: 'Email or password is incorrect' },
         })
+      } else {
+        const id = results[0].id
+
+        const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRES_IN,
+        })
+
+        console.log('The token is', token)
+
+        const cookieOptions = {
+          expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+          httpOnly: true,
+        }
+
+        res.cookie('jwt', token, cookieOptions)
+        res.status(200).redirect('/')
       }
     })
-  })
+  } catch (error) {
+    console.log(error)
+  }
 }
