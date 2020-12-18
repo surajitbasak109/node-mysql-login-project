d52c84db8493aaf1f0ec93e3fcd7c81ad9853120
Surajit Basak <surajitbasak109@gmail.com>
2020-12-19 02:02:19 +0530

    user helper added to get user details from jwt token

---
+++ helper/user_helper.js
@@ -0,0 +1,17 @@
+const db = require('../database')
+const jwt = require('jsonwebtoken')
+
+exports.getUser = (token, cb) => {
+  const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  const userId = decoded.id
+
+  db.query('SELECT * FROM users WHERE id = ?', [userId], (err, results) => {
+    if (err) {
+      console.log(err)
+    }
+
+    if (results.length > 0) {
+      return cb(results[0])
+    }
+  })
+}
